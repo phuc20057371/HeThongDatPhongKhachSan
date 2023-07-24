6998f37b87f1018da3e0e1cbcd0a15d3925a0a30
Nguyen Huynh Huong <huynhhuong205k@gmail.com>
2023-07-24 21:36:04 +0700

    api gateway

---
+++ security-service/src/main/java/com/example/securityservice/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.securityservice.controller;
+
+import com.example.securityservice.dto.AuthRequest;
+import com.example.securityservice.entity.UserCredential;
+import com.example.securityservice.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public String addNewUser(@RequestBody UserCredential user){
+        return authService.saveUser(user);
+    }
+    @PostMapping("/token")
+    public String getToken(@RequestBody AuthRequest authRequest){
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if(authenticate.isAuthenticated()){
+            return authService.generateToken(authRequest.getUsername());
+        }
+        else{
+            throw new RuntimeException("invalid access");
+        }
+
+    }
+    @GetMapping("/validate")
+    public String validateToken(@RequestParam("token") String token){
+        authService.validateToken(token);
+        return "Token is valid";
+    }
+}
---
+++ security-service/src/main/java/com/example/securityservice/service/AuthService.java
@@ -0,0 +1,29 @@
+package com.example.securityservice.service;
+
+
+import com.example.securityservice.entity.UserCredential;
+import com.example.securityservice.repository.UserCredentialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserCredentialRepository userCredentialRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+    public String saveUser(UserCredential userCredential){
+        userCredential.setPassword(passwordEncoder.encode(userCredential.getPassword()));
+        userCredentialRepository.save(userCredential);
+        return "save user !";
+    }
+    public String generateToken(String userName){
+        return jwtService.generateToken(userName);
+    }
+    public void validateToken(String token){
+        jwtService.validateToken(token);
+    }
+}
